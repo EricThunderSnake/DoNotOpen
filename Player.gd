a19d430996d492fe441ad13389189f3f0d582b3e
Vej333 <garnier.vej@gmail.com>
2025-01-20 22:46:18 +0100

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -1,28 +1,29 @@
 extends CharacterBody3D
 
+@onready var gun_controller = $GunController
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+var speed = 8
+#Executed every frames:
+func _process(delta):
+	
+	
+	
+	#Movement
+	velocity = Vector3()
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.z -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.z += 1
+	
+	velocity = velocity.normalized()*speed
 	move_and_slide()
+	
+	#Shooting
+	if Input.is_action_pressed("shoot"):
+		print("shooting")
+		gun_controller.shoot()
