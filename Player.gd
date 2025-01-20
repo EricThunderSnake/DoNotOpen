65ce361dbe63a108b44de09f99f6ecd46ce0e522
206417 <58572765+EricThunderSnake@users.noreply.github.com>
2025-01-20 22:02:00 +0000

    Reorganised scene tree
    Shooting now works as intended

--- Player.gd
+++ Player.gd
@@ -7,23 +7,23 @@ var speed = 8
 func _process(delta):
 	
 	
-	
+
 	#Movement
 	velocity = Vector3()
 	
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("MoveRight"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("MoveLeft"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("MoveForward"):
 		velocity.z -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("MoveBackward"):
 		velocity.z += 1
 	
 	velocity = velocity.normalized()*speed
 	move_and_slide()
 	
 	#Shooting
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_pressed("Interact"):
 		print("shooting")
 		gun_controller.shoot()
