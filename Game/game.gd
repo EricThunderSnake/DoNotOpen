d1fbca6d0d1898676620fa5ff0b0df2a40ca0992
206417 <58572765+EricThunderSnake@users.noreply.github.com>
2025-02-13 01:35:17 +0000

    Final pt4
    This is the game as it was uploaded.

--- Game/game.gd
+++ Game/game.gd
@@ -2,16 +2,17 @@ extends WorldEnvironment
 
 
 @onready var pause_menu = $"Pause Menu"
+@onready var start_dialogue = $"Start Dialogue"
 
 var paused = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Pause"):
+	if Input.is_action_just_pressed("Escape") && !start_dialogue.visible:
 		pauseMenu()
 
 
 func pauseMenu():
-	if not paused:
+	if not paused or pause_menu == null:
 		pause_menu.show()
 		Engine.time_scale = 0
 	else:
